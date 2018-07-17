224c57c0d92beef6eae44cd75c972698351a4dc2
林士立 <t301000@gmail.com>
2018-07-17 14:49:24 +0800

    匯入名單

---
+++ do-import.php
@@ -0,0 +1,51 @@
+<?php
+    require_once __DIR__ . '/bootstrap.php';
+
+    // 沒有檔案
+    if (!isset($_FILES['list'])) {
+        header('Location: import.php');
+        die();
+    }
+
+    // 有檔案
+    $file = $_FILES['list'];
+    // var_dump($file);
+
+    $origin_name = $file['name'];
+    $tmp_array = explode('.', $origin_name);
+    $ext = end($tmp_array);
+
+    // 不是 csv 檔
+    if ($ext !== 'csv') {
+        header('Location: import.php');
+        die();
+    }
+
+    // 處理 csv 檔
+    $tmp_file = $file['tmp_name'];
+    $handler = fopen($tmp_file, 'r');
+    // 取出第1行標題，不處理
+    $line_array = fgetcsv($handler, 1000);
+
+    // $sql = "INSERT INTO students (class, cnum, name, gender) VALUES ('$class', $cnum, '$name', '$gender')";
+    $sql = "INSERT INTO students (class, cnum, name, gender) VALUES (:class, :cnum, :name, :gender)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':class', $class);
+    $stmt->bindParam(':cnum', $cnum);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':gender', $gender);
+
+    while($line_array = fgetcsv($handler, 1000)) {
+        // var_dump($line_array);
+        $class = $line_array[0];
+        $cnum = (int) $line_array[1];
+        $name = $line_array[2];
+        $gender = $line_array[3];
+
+        $stmt->execute();
+    }
+
+    header('Location: import.php');
+
+
+
---
+++ import.php
@@ -0,0 +1,42 @@
+<?php
+    require_once __DIR__ . '/bootstrap.php';
+    require_once __DIR__ . '/partials/header.php';
+?>
+
+
+        <main role="main" class="container">
+            <div class="row">
+                <!-- 主要內容區 -->
+              <div class="col-12 col-sm-9">
+                <div class="row">
+                  <!-- 班級名單 -->
+                  <div class="col-12">
+                    <h2 class="text-center">匯入名單</h2>
+
+
+                    <form action="do-import.php" method="post" enctype="multipart/form-data">
+                      <input type="file" name="list">
+                      <button type="submit">匯入</button>
+                    </form>
+
+
+
+                  </div>
+                </div>
+              </div>
+
+                <!-- 側邊欄 -->
+                <?php require_once __DIR__ . '/partials/sidebar.php'; ?>
+            </div>
+        </main>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="./assets/jquery/jquery-3.3.1.slim.min.js"></script>
+        <script src="./assets/popper/popper.min.js"></script>
+        <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
+
